b8e982528d5f6acfa56fa3fae072b8210ef89a81
Grzegorz Grzybek <gr.grzybek@gmail.com>
2019-08-20 12:50:59 +0200

    [C, Java] Working JNI example with method implemented in Eclipse/CDT

---
+++ java/src/test/java/grgr/f2f2019/NativeTest.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package grgr.f2f2019;
+
+import grgr.f2f2019.n.NativeBridge;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class NativeTest {
+
+    private static NativeBridge NB;
+
+    @BeforeAll
+    public static void init() {
+        NB = new NativeBridge();
+    }
+
+    @Test
+    public void invokeJavaMethods() {
+        NB.helloJava("Grzegorz");
+    }
+
+    @Test
+    public void invokeNativeMethods() {
+        NB.helloNative("Grzegorz");
+    }
+
+}
